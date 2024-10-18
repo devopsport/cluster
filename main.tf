6dd26ae50eb5c58416af2981a6856b61cf116b56
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-17 22:19:53 -0500

    agregando acl

--- main.tf
+++ main.tf
@@ -8,6 +8,20 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "main" {
   bucket = aws_s3_bucket.main.id
 
